e81745e8b30ec54a38f906791e81a56e604ef980
siya824 <siya20251085@miis.ae>
2026-05-05 11:20:13 +0400

    Implement linear regression model for exam score prediction

---
+++ linear.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+df=pd.read_csv("data.csv")
+X=df[['HoursStudied']]
+y=df['ExamScore']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model=LinearRegression()
+model.fit(X_train, y_train)
+st.title("Exam Score Predictior")
+st.write("Enter hours studied to predict the exam score.")
+hours=st.number_input("Hours Studied:", min_value=0.0, step=0.1)
+if st.button("Predict Score"):
+  predicted_score=model.predict([[hours]])[0]
+  st.success(f"Predicted Scores: {predicted_score: .2f}")
+  st.write("Smaple training data")
+  st.dataframe(df)
