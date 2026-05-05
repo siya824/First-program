3cb3d5d4b3d7c6682336212e9ccb7d592fba6898
siya824 <siya20251085@miis.ae>
2026-05-05 11:34:27 +0400

    Fix typo in pandas read_csv function

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df=pd.data.csv("data.csv")
+df=pd.read_csv("data.csv")
 X=df[['HoursStudied']]
 y=df['ExamScore']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
