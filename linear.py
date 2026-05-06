3213e70c69b8f092d9814b7d86075273b93a2d48
siya824 <siya20251085@miis.ae>
2026-05-06 12:31:09 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,18 +1,18 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd.
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df=pd.read_csv("data(2).csv")
-X= df.iloc[:, :-1].values 
-Y= df.iloc[:, -1].values
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-model=LinearRegression()
+df pd.read_csv("data(2).csv")
+X= df.iloc[:, :-1].values # features
+Ydf.iloc[:, -1].values
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+model LinearRegression()
 model.fit(X_train, Y_train)
-st.title("Exam Score Predictior")
-st.write("Enter hours studied to predict the exam score.")
-hours=st.number_input("Hours Studied:", min_value=0.0, step=0.1)
+st.title("Exam Score Prediction Model")
+st.write("Enter the no. of hours you are studied for the exam")
+hours st.number_input("Hours Studied", min_value=0.0, step = 0.1)
 if st.button("Predict Score"):
-  predicted_score=model.predict([[Hours]])[0]
-  st.success(f"Predicted Scores: {predicted_score: .2f}")
-  st.write("Smaple training data")
-  st.dataframe(df)
+predicted_score model.predict([[hours]])[0]
+st.success(f"Predicted Score: (predicted_score: .2f}")
+st.write("Sample Training DATA")
+st.dataframe(df)
