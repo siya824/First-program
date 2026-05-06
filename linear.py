69c11fca72d1c8c7318a6eef83ab2bd8e5b2d022
siya824 <siya20251085@miis.ae>
2026-05-06 12:40:24 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df=pd.read_csv("data(2).csv")
+df=pd.read_csv("students_scores.csv")
 X= df.iloc[:, :-1].values 
 Y= df.iloc[:, -1].values
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
