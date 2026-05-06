427ec51359e387ecc826e303636d7ba21d1a757d
siya824 <siya20251085@miis.ae>
2026-05-06 12:47:20 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 df=pd.read_csv("students_scores.csv")
 X= df.iloc[:, :-1].values 
 Y= df.iloc[:, -1].values
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 22)
 model=LinearRegression()
 model.fit(X_train, Y_train)
 st.title("Exam Score Prediction Model")
