c0c1c814d531157c36309a671c9ac25a10a20faa
siya824 <siya20251085@miis.ae>
2026-05-06 12:27:57 +0400

    Fix data extraction for features and target variable

--- linear.py
+++ linear.py
@@ -3,8 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df=pd.read_csv("data(2).csv")
-X= df.iloc[:, :-1].values [['HoursStudied']]
-Y= df.iloc[:, -1].values['ExamScore']
+X= df.iloc[:, :-1].values 
+Y= df.iloc[:, -1].values
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 model=LinearRegression()
 model.fit(X_train, Y_train)
